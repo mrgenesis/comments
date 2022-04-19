cae197e24d0b1f38e7e46973b83340a864fdd0d6
Rosa, Genesis <mrgenesis@hotmail.com>
2022-04-19 16:47:24 -0400

    O componente de login foi adiciondo por volta do App

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
+import Login from './components/Login';
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-
 root.render(
   <React.StrictMode>
-    <App />
+    <Login>
+      <App />
+    </Login>
   </React.StrictMode>
 );
-
