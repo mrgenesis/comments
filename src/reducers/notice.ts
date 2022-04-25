910eaf92a8d82b356c4f281443b4ce79d3c372df
Rosa, Genesis <mrgenesis@hotmail.com>
2022-04-25 09:32:43 -0400

    Criados reducer das  mensagens do sistema.

---
+++ src/reducers/notice.ts
@@ -0,0 +1,23 @@
+
+import { INotice, NoticeAction } from '../interfaces';
+
+export default function noticeReducer(state: INotice, action: NoticeAction): INotice {
+  const resetState: INotice = {
+    "message": "",
+    "hiddenStatus": true,
+    "severity": "info",
+    "children": undefined
+  }
+  switch (action.type) {
+    case 'SUCCESS':
+      return { ...state, hiddenStatus: false, severity: "success", ...action?.payload };
+    case 'ERROR':
+      return { ...state, hiddenStatus: false, message: 'error' };
+    case 'GENERIC':
+      return { ...resetState, ...action.payload };
+    case 'RESET':
+      return { ...resetState };
+    default:
+      throw new Error(`The type "${action.type}" <action.type> don't exist.`);
+  }
+}
