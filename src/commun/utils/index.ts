9a1739749659b0b1a0f09d2d65861cef1d4589c0
Rosa, Genesis <mrgenesis@hotmail.com>
2022-04-26 18:10:04 -0400

    Criado arquivo para exportar todos os utilitários.

---
+++ src/commun/utils/index.ts
@@ -0,0 +1,15 @@
+
+import { uid } from './uid';
+import { convert } from './convert';
+import { dataTypes } from './types';
+import { nextOfQueue } from './nextOfQueue';
+import { addOrDecreaseOne } from './addOrDecreaseOne';
+
+
+export { 
+  uid,
+  convert,
+  dataTypes,
+  nextOfQueue,
+  addOrDecreaseOne,
+ };
