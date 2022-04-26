d3edbbf15d240e0568724bb5342e21f084b1181a
Rosa, Genesis <mrgenesis@hotmail.com>
2022-04-26 18:39:34 -0400

    Configurada as rotas.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,21 @@
+import LoaderStorage from "../storage/loader";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import LoaderStorage from "../storage/loader";
+
+import Home from './Home';
+import Comments from './Comments';
+
 
 export default function Root() {
   return (
     <LoaderStorage>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="comments/:clientId" element={<Comments />} />
         </Routes>
       </BrowserRouter>
     </LoaderStorage>
