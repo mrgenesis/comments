2fb81dd445f72b3664d0eefd55f001ed384bb6e1
Rosa, Genesis <mrgenesis@hotmail.com>
2022-04-28 11:05:01 -0400

    Exibindo os dados de configuração da lista no title do chip.

--- src/components/List/DisplayLists.tsx
+++ src/components/List/DisplayLists.tsx
@@ -12,14 +12,19 @@ const DisplayLists:  React.FunctionComponent<{ lists: IList[] }>  = ({ lists }) 
     localStorage.removeItem(list.name);
     localStorage.setItem(list.name, JSON.stringify(list));
     const getRegistry = addOrDecreaseOne(list);
-    navegate(`/comments/${getRegistry('more')}?listId=${list.name}`);
+    navegate(`/comments/${getRegistry('more')}?listId=${list.name}&beginning=${list.beginning}&end=${list.end}&isPhoneNumberId=${list.isPhoneNumberId}`);
+  }
+  
+  const generateTitel = {
+    'Sequencial': (dataList: IList) => `Início: ${dataList.beginning}; Fim: ${dataList.end}; Gera link do WhatsApp: ${(dataList.isPhoneNumberId === '1') ? 'Sim' : 'Não'}`
   }
   return (
     <>
       <p>Clique em uma das listas.</p>
       {lists.map(list => {
         return (
-          <UIChip 
+          <UIChip
+          title={generateTitel['Sequencial'](list)} 
           key={uid()} 
           sx={{ margin: 0.3 }} 
           onClick={() => handleSelectedList(list)} 
